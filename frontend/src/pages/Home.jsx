532deb464da9e96f6e1fe02fa336ed8029d8604f
roshan749997 <roshanchaudhari4145@gmail.com>
2026-03-25 12:54:59 +0530

    change pink to green

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,15 +12,23 @@ const Home = () => {
       <HeroSlider
         slides={[
           {
-            desktop: 'https://res.cloudinary.com/dvkxgrcbv/image/upload/v1765533980/Cream_Green_Playful_Kids_Fashion_Trends_Twitter_Header_2048_x_594_px_jsjjsh.png',
+            desktop: 'https://res.cloudinary.com/dzd47mpdo/image/upload/v1774422260/Red_and_Green_Modern_Beverage_Drink_Instagram_Post_1080_x_1080_px_1920_x_600_px_peybb9.png',
             alt: 'Kidzo - Premium Kids & Baby Products Collection',
           },
           {
-            desktop: 'https://res.cloudinary.com/dvkxgrcbv/image/upload/v1765533979/Green_Cute_Baby_Store_Facebook_Ads_2048_x_594_px_tiow9i.svg',
+            desktop: 'https://res.cloudinary.com/dzd47mpdo/image/upload/v1774422272/Untitled_1920_x_600_px_16_qkdbcw.png',
+            alt: 'Festive Season Offer - Kidzo',
+          },
+          {
+            desktop: 'https://res.cloudinary.com/dzd47mpdo/image/upload/v1774422686/Banner_Grocery_Delivery_Fresh_Organic_1920_x_600_px_hxivu9.png',
+            alt: 'Festive Season Offer - Kidzo',
+          },
+          {
+            desktop: 'https://res.cloudinary.com/dzd47mpdo/image/upload/v1774422896/Yellow_Fast_Food_Fest_Website_Banner_1920_x_600_px_bbseqi.png',
             alt: 'Festive Season Offer - Kidzo',
           },
         ]}
-        mobileSrc="https://res.cloudinary.com/dvkxgrcbv/image/upload/v1765533983/Beige_Neutral_Kids_Fashion_Product_Detail_Instagram_Post_obnvdg.svg"
+        mobileSrc="https://res.cloudinary.com/dzd47mpdo/image/upload/v1774422686/White_and_Green_Grocery_Store_Instagram_Post_600_x_600_px_e93fw4.png"
       />
 
       {/* Kidzo Sections */}
